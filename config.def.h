f3ec4175f495c4560383719c25f21f2d60c3193d
Gabriel Acosta <acostadariogabriel@gmail.com>
2022-05-16 08:49:41 -0300

    add ligatures

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Cascadia Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "Cascadia Code:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*
