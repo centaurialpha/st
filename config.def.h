1cd6d7bb6a2f43fcc7343cf1e9e547107b8788bf
Gabriel Acosta <acostadariogabriel@gmail.com>
2022-10-08 14:53:42 -0300

    fix fix

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hermit:pixelsize=24:antialias=true:autohint=true";
+static char *font = "Cascadia Code:pixelsize=24:antialias=true";
 static int borderpx = 5;
 
 /*
